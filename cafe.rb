5e77406b7e80b73e58413ddf79e92991c9ee1a97
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-09-02 09:29:00 +0900

    cafeプログラム作成

--- cafe.rb
+++ cafe.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'debug'
+
 DRINKS = [
   { name: 'コーヒー', price: '300' },
   { name: 'カフェラテ', price: '400' },
@@ -15,12 +17,14 @@ FOODS = [
 ].freeze
 
 def take_order(menus)
+#  binding.break
   menus.each.with_index(1) do |menu, i|
-    puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
+    puts "(#{i})#{menu[:name]}: #{menu[:price].to_i}円"
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
+  binding.break
+  puts "#{menus[order_number][:name]}(#{menus[order_number][:price].to_i}円)ですね。"
   order_number
 end
 
@@ -30,5 +34,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = FOODS[order1][:price].to_i+ DRINKS[order2][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
