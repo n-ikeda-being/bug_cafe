a3c93d1b48813ec59e61817b2449fee9f725fcb6
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-09-02 09:44:42 +0900

    cafeプログラム修正ー１回目

--- cafe.rb
+++ cafe.rb
@@ -1,38 +1,32 @@
-# frozen_string_literal: true
-
-require 'debug'
-
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
-  { name: 'カフェラテ', price: '400' },
-  { name: 'チャイ', price: '460' },
-  { name: 'エスプレッソ', price: '340' },
-  { name: '緑茶', price: '450' }
+  { name: 'コーヒー', price: 300 },
+  { name: 'カフェラテ', price: 400 },
+  { name: 'チャイ', price: 460 },
+  { name: 'エスプレッソ', price: 340 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
-  { name: 'アップルパイ', price: '520' },
-  { name: 'ホットサンド', price: '410' }
+  { name: 'チーズケーキ', price: 470 },
+  { name: 'アップルパイ', price: 520 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
-#  binding.break
   menus.each.with_index(1) do |menu, i|
-    puts "(#{i})#{menu[:name]}: #{menu[:price].to_i}円"
+    puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  order_number = gets.to_i
-  binding.break
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price].to_i}円)ですね。"
+  order_number = gets.to_i - 1
+  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food = take_order(FOODS)
 
-total = FOODS[order1][:price].to_i+ DRINKS[order2][:price].to_i
+total = DRINKS[drink][:price] + FOODS[food][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
